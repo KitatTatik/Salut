c3d28b5ad877fa221dfb30e9e48f66173122d6da
Vasiliy Tolstoy <vmt_bat@mail.ru>
2022-09-14 08:17:16 -0700

    Made it working again!

--- fr2.c
+++ fr2.c
@@ -19,6 +19,7 @@ int main()
     do
     {
         q = getchar();
+        printf("DEBUG q = %c, i = %d\n", q, i);
         if (isalnum(q) || ' '==q)
         {
             if (isalpha(q))
@@ -27,36 +28,36 @@ int main()
                 string[i++] = q;
         } 
     }
-    while (q != '\n'); // TODO: deal with endless strings
-
+    while (q != '\n' && i < sizeof(string));
     string[i] = '\0';
 
-    for (i = 0, j = 0; i<strlen(string); ) {
+    for (i = 0, j = 0; i<strlen(string); i++) {
         if (string[i] == ' ' )
         {
             if (!j) continue;
             if (string[i+1] == ' ') continue;
         }
             
-        result[j++] = string[i++];
+        result[j++] = string[i];
     }
+    result[j] = '\0';
+    
     printf("The modified text is: %s\n", result);
 
-    for (i = 0, j = 0; i < strlen(result); ) // mb res_len = strlen(result); and so on?
+    for (i = 0, j = 0; i < strlen(result); i++) // mb res_len = strlen(result); and so on?
     {
-        while (result[i] != ' ')
+        if (result[i] != ' ')
         {
-            string[j++] = result[i++];
-//          printf("Debug print %d.\n", j);
-
+            string[j++] = result[i];
+//            printf("DEBUG %d.\n", j);
         }
-
-        if (j != 0)
+        else if (j)
         {
             count++;
             j = 0;
         }
     }
+    if (j) count++;
     
     printf("The number of  words in the text is %d.\n", count);
 }
